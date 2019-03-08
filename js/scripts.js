e89c62ace8ae58cad26d3a7c57f2dec0f22fd64c
gracety <grace.wongty@gmail.com>
2019-03-07 16:19:53 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 // Parse user input
 function kyleReplies(){	
 
+	var luckyNum = Math.floor(Math.random() * 5);
+
+		console.log("lucky number:" + luckyNum);
+
+		if (luckyNum == 3){
+			document.getElementById('demo').innerHTML = "Fuck.";
+		} else {
+
 	// grab the user's input
 	var sentence = $('#userInput').val();
 
@@ -15,7 +23,7 @@ function kyleReplies(){
 	for (var i = 0; i < len; i++) {
 
 		// Check if the loop statement prints
-		console.log(i);
+		// console.log(i);
 		
 		// replace key words
 		switch(wordArray[i]){
@@ -23,41 +31,57 @@ function kyleReplies(){
 			// replace "me" with "you"
 			case "me": 				
 				wordArray.splice(i, 1, "you");
+				// Background change
+				document.body.style.backgroundImage = "url('images/img12.jpg')";
 				break;
 
 			// replace "my" with "your"
 			case "my": 				
 				wordArray.splice(i, 1, "your");
+				// Background change
+				document.body.style.backgroundImage = "url('images/meagain.jpg')";
 				break;
 
 			// replace "I" with "you"
 			case "I": 				
 				wordArray.splice(i, 1, "you");
+				// Background change
+				document.body.style.backgroundImage = "url('images/img12.jpg')";
 				break;
 
 		 	// replace "i" with "you"
 			case "i": 				
 				wordArray.splice(i, 1, "you");
+				// Background change
+				document.body.style.backgroundImage = "url('images/meagain.jpg')";
 				break;
 
 			// replace "I'm" with "are you"
 			case "I'm": 				
 				wordArray.splice(i, 1, "are you");
+				// Background change
+				document.body.style.backgroundImage = "url('images/img12.jpg')";
 				break;
 
 			// replace "i'm" with "are you"
 			case "i'm": 				
 				wordArray.splice(i, 1, "are you");
+				// Background change
+				document.body.style.backgroundImage = "url('images/meagain.jpg')";
 				break;
 
 			// replace "am" with "are"
 			case "am": 				
 				wordArray.splice(i, 1, "are");
+				// Background change
+				document.body.style.backgroundImage = "url('images/img12.jpg')";
 				break;
 
 			// replace "mine" with "yours"
 			case "mine": 				
 				wordArray.splice(i, 1, "yours");
+				// Background change
+				document.body.style.backgroundImage = "url('images/meagain.jpg')";
 				break;
 
 			// otherwise the default will be...
@@ -69,12 +93,12 @@ function kyleReplies(){
 
 	} // for loop end bracket
 
-	console.log(wordArray);
-
 	// Display response i..e result of key word replacement
 
 	var res = wordArray.join(" ");
-	document.getElementById("demo2").innerHTML = res + "?";
+	document.getElementById("demo").innerHTML = res + "?";
+
+	} // end bracket for else
 
 } // kyleReplies function end bracket
 
